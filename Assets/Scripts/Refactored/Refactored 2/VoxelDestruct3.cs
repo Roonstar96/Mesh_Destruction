72b1318334dc73ef978a3e4fabc35cde7bd8d66a
Roonstar96 <tane2.cotterell-east@live.uwe.ac.uk>
2023-01-08 09:06:07 +0000

    Mesh Destruction Git repo update

--- Assets/Scripts/Refactored/Refactored 2/VoxelDestruct3.cs
+++ Assets/Scripts/Refactored/Refactored 2/VoxelDestruct3.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class VoxelDestruct3 : MonoBehaviour
 {
+
+    //NOTE: All variables for the object being destroyed, its debris
+    // & other variables for instantiating the debris in the correct position 
+
     [SerializeField] private GameObject parent;
     [SerializeField] private GameObject newPiece;
-    [SerializeField] private float newScale;
+    //[SerializeField] private float newScale;
     //[SerializeField] private int row;
 
     [SerializeField] public float rowX;
@@ -16,8 +20,14 @@ public class VoxelDestruct3 : MonoBehaviour
     [SerializeField] public float newScaleX;
     [SerializeField] public float newScaleY;
     [SerializeField] public float newScaleZ;
+
+    [SerializeField] private float pScaleX;
+    [SerializeField] private float pScaleY;
+    [SerializeField] private float pScaleZ;
     //[SerializeField] private float pieceScaleFactor;
 
+
+    //NOTE: define the varibles for instantiaing the pieces ones parent obejct is destroyed
     private void Start()
     {
         rowX = parent.transform.localScale.x;
@@ -27,6 +37,10 @@ public class VoxelDestruct3 : MonoBehaviour
         newScaleX = newPiece.transform.localScale.x;
         newScaleY = newPiece.transform.localScale.y;
         newScaleZ = newPiece.transform.localScale.z;
+
+        pScaleX = (parent.transform.localScale.x) / 2;
+        pScaleY = (parent.transform.localScale.y) / 2;
+        pScaleZ = (parent.transform.localScale.z) / 2;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -41,11 +55,11 @@ public class VoxelDestruct3 : MonoBehaviour
     {
         parent.SetActive(false);
 
-        for (float x = 0; x < rowX; x+= newScale)
+        for (float x = 0; x < rowX; x+= newScaleX)
         {
-            for (float y = 0; y < rowY; y+= newScale)
+            for (float y = 0; y < rowY; y+= newScaleY)
             {
-                for (float z = 0; z < rowZ; z += newScale)
+                for (float z = 0; z < rowZ; z += newScaleZ)
                 {
                     CreatePieces(x, y, z);
                 }
@@ -70,8 +84,11 @@ public class VoxelDestruct3 : MonoBehaviour
         pieces = GameObject.Instantiate(newPiece, parent.transform.position, Quaternion.identity);
         //pieces = GameObject.CreatePrimative(PrimitiveType.Cube); 
 
-        pieces.transform.position = transform.position + new Vector3(newScale + x, newScale + y, newScale + z);
-        pieces.transform.localScale = new Vector3(newScale, newScale, newScale);
+        pieces.transform.position = transform.position + new Vector3(
+            (newScaleX + x) - pScaleX, 
+            (newScaleY + y) - pScaleY, 
+            (newScaleZ + z) - pScaleZ);
+        //pieces.transform.localScale = new Vector3(newScaleX, newScaleY, newScaleZ);
 
         pieces.AddComponent<MeshRenderer>();
         //pieces.AddComponent<Rigidbody>();
